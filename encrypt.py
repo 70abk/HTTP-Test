777f41a859653f8b5d486e2a9e1d59330138e9c3
70abk <h71abk@gmail.com>
2026-03-30 13:29:30 +0900

    Work in progress

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,7 @@
 import math
 
 class sequence:
+    # a(n) = func(a(n-1))
     def __init__(self, func, init=1):
         self.seq = [init]
         self.index = 1
@@ -8,14 +9,84 @@ class sequence:
     def call(self, index):
         if type(index) != int:
             raise TypeError("Index must be an integer")
-            return None
         while self.index <= index:
             self.seq.append(self.func(self.seq[-1]))
             self.index += 1
         return self.seq[index-1]
         
-sequence1 = sequence(lambda x: 2*x*math.sin(x) + x*math.cos(x**2), init=30) # (x^3)*sin(x) + x*cos(x^2)
+def keygen_single(func, init, n=20):
+    seq = sequence(func, init=init)
 
-if __name__ == "__main__":
-    print(sequence1.call(50))
-    print (sequence1.seq)
+    y = seq.call(n)
+
+    public_key = {
+        "n": n,
+        "y": abs(int(y)) % 256,
+        "func": func
+    }
+
+    private_key = {
+        "init": init,
+        "func": func
+    }
+
+    return public_key, private_key
+
+pub1, priv1 = keygen_single(
+    lambda x: 2*x*math.sin(x) + x*math.cos(x**2), # 2x*sin(x) + x*cos(x^2)
+    init=15
+)
+
+pub2, priv2 = keygen_single(
+    lambda x: x + math.log(2 + (x**3) * math.exp(-x)), # x + log(2 + (x^3)*e^(-x))
+    init=1
+)
+
+pub3, priv3 = keygen_single(
+    lambda x: 2*x + 3, # 2x + 3
+    init=1
+)
+
+def encrypt(message, public_key, func):
+    current = public_key["y"]  # 시작값 (공개키에서 가져옴)
+
+    encrypted = ""
+    for c in message:
+        k = abs(int(current)) % 256
+        encrypted += chr((ord(c) + k) % 256)
+
+        # 다음 키 생성
+        current = func(current)
+
+    return encrypted
+
+def decrypt(cipher, private_key, n):
+    seq = sequence(private_key["func"], init=private_key["init"])
+
+    # 시작값 복원 (k₀)
+    current = seq.call(n)
+
+    message = ""
+    for c in cipher:
+        k = abs(int(current)) % 256
+        message += chr((ord(c) - k) % 256)
+
+        # 동일하게 다음 키 생성
+        current = private_key["func"](current)
+
+    return message
+
+msg = "Hello"
+
+enc1 = encrypt(msg, pub1, priv1["func"])
+dec1 = decrypt(enc1, priv1, pub1["n"])
+
+enc2 = encrypt(msg, pub2, priv2["func"])
+dec2 = decrypt(enc2, priv2, pub2["n"])
+
+enc3 = encrypt(msg, pub3, priv3["func"])
+dec3 = decrypt(enc3, priv3, pub3["n"])
+
+print(enc1, dec1)
+print(enc2, dec2)
+print(enc3, dec3)
--- index.js
+++ index.js
@@ -2,10 +2,126 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
-const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const cookieParser = require('cookie-parser');
+
+// sequence 클래스
+class Sequence {
+    constructor(func, init = 1) {
+        this.seq = [init];
+        this.index = 1;
+        this.func = func;
+    }
+
+    call(index) {
+        if (!Number.isInteger(index)) {
+            throw new TypeError("Index must be an integer");
+        }
+
+        while (this.index <= index) {
+            this.seq.push(this.func(this.seq[this.seq.length - 1]));
+            this.index++;
+        }
+
+        return this.seq[index - 1];
+    }
+}
+
+// 키 생성
+function keygenSingle(func, init, n = 20) {
+    const seq = new Sequence(func, init);
+    seq.call(n);
+
+    const publicKey = {
+        n: n,
+        func: func
+    };
+
+    const privateKey = {
+        init: init,
+        func: func
+    };
+
+    return { publicKey, privateKey };
+}
+
+// 수열 정의
+const { publicKey: pub1, privateKey: priv1 } = keygenSingle(
+    x => 2 * x * Math.sin(x) + x * Math.cos(x * x),
+    15
+);
+
+const { publicKey: pub2, privateKey: priv2 } = keygenSingle(
+    x => x + Math.log(2 + (x ** 3) * Math.exp(-x)),
+    1
+);
+
+const { publicKey: pub3, privateKey: priv3 } = keygenSingle(
+    x => 2 * x + 3,
+    1
+);
+
+function next(current, func) {
+    let x = func(current);
+    return Math.abs(Math.floor(x)) % 256;
+}
+
+// 암호화
+function encrypt(message, publicKey) {
+    const start = Math.floor(Math.random() * 256);
+    let current = start;
+    const func = publicKey.func;
+
+    const result = [];
+
+    for (let i = 0; i < message.length; i++) {
+        const k = current;
+        result.push((message.charCodeAt(i) + k) % 256);
+
+        current = next(current, func); // 핵심 수정
+    }
+
+    return {
+        start,
+        cipher: Buffer.from(result)
+    };
+}
+
+// 복호화
+function decrypt(encrypted, privateKey, n) {
+    const seq = new Sequence(privateKey.func, privateKey.init);
+    const seedValue = Math.abs(Math.floor(seq.call(n))) % 256;
+    const cipherBuffer = encrypted.cipher;
+
+    let current = (encrypted.start + seedValue) % 256;
+
+    let result = "";
+
+    for (let i = 0; i < cipherBuffer.length; i++) {
+        const k = current;
+        result += String.fromCharCode((cipherBuffer[i] - k + 256) % 256);
+
+        current = next(current, privateKey.func); // 동일하게
+    }
+
+    return result;
+}
+
+// 테스트
+const msg = "Hello";
+
+const enc1 = encrypt(msg, pub1);
+const dec1 = decrypt(enc1, priv1, pub1.n);
+
+const enc2 = encrypt(msg, pub2);
+const dec2 = decrypt(enc2, priv2, pub2.n);
+
+const enc3 = encrypt(msg, pub3);
+const dec3 = decrypt(enc3, priv3, pub3.n);
+
+console.log("enc1:", enc1.cipher.toString("hex"), "| start1:", enc1.start, "| dec1:", dec1);
+console.log("enc2:", enc2.cipher.toString("hex"), "| start2:", enc2.start, "| dec2:", dec2);
+console.log("enc3:", enc3.cipher.toString("hex"), "| start3:", enc3.start, "| dec3:", dec3);
 
 const app = express();
 app.use(cors());
@@ -24,15 +140,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.get('/set-cookie', (req, res) => {
-    res.cookie('session_id', 'user12345', {
-        httpOnly: true,        // JS에서 접근 불가
-        secure: true,          // HTTPS에서만 전송
-        sameSite: 'Strict',    // 외부 요청에서 쿠키 전송 금지
-        maxAge: 1000 * 60 * 10 // 10분 유지
-    });
-    res.send('보안 쿠키가 설정되었습니다.');
-});
 
 // index.html 전송
 app.get('/', (req, res) => {
@@ -55,11 +162,4 @@ http.createServer(app).listen(3000, () => {
     console.log('HTTP  서버:  http://localhost:3000');
 });
 
-const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'certs', 'server.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'certs', 'server.crt'))
-};
 
-https.createServer(httpsOptions, app).listen(3443, () => {
-    console.log('HTTPS 서버: https://localhost:3443');
-});
